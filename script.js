54bade4b4a8efbfc3e20c7c7a568eb1042e0a83f
adelajahalima <adelajahalima@gmail.com>
2024-06-29 16:07:28 +0100

    My first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Get the current year
+var currentYear = new Date().getFullYear();
+  
+// Set the current year in the span element
+document.getElementById("currentYear").textContent = currentYear;
